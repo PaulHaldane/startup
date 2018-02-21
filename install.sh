5fcb1eacb772ed9c6731d9620e25c109cdb1bdf8
Paul Haldane <Paul.Haldane@gmail.com>
2018-02-21 14:30:48 +0000

    Update install.sh
    Don't blindly overwrite an existing .ssh/authorized_keys file (as
    this will specifically lose ssh access to AWS instances).
    Instead we have the keys we want to add in a separate file and 
    compare the combined, sorted contents of extra file and existing file
    with the installed one. We only update the installed file if there's
    a difference.

--- install.sh
+++ install.sh
@@ -6,7 +6,24 @@ if [ ! -f README.md ]; then
 fi
 
 tar --exclude README.md --exclude .git -cpBvf - . | ( cd ~ && tar xpBf - )
+
 cd
 chmod 755 . .ssh
 cd .ssh
-chmod 600 config authorized_keys
+
+akeys=authorized_keys
+akeys_combined=${akeys}_$$
+akeys_add=${akeys}_add
+
+if [ -s $akeys ]; then
+	cat $akeys_add $akeys | sort -u > $akeys_combined
+	if diff $akeys $akeys_combined > /dev/null ; then
+		rm $akeys_combined
+	else
+		mv $akeys_combined $akeys
+	fi
+else
+	mv $akeys_add $akeys
+fi
+
+chmod 600 config $akeys
